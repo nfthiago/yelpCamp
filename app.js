6890ded06c8df1b41c2be797e925a2665b5b4232
nfthiago <nfthiago@gmail.com>
2021-08-26 12:32:12 +0100

    :seedling: Add seed file

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
-})
+});
 
 const app = express();
 
