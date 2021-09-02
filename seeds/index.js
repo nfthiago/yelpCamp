be11f63e0850dc1565c258f97f36e44b7e7a9735
nfthiago <nfthiago@gmail.com>
2021-09-02 15:35:28 +0100

    :art: Add img, description and price to campground model

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex similique itaque deleniti, dignissimos ipsum velit placeat soluta fugit odit maiores. Nostrum illum perspiciatis temporibus cum consequuntur laudantium facere nulla harum!',
+            price
         })
         await camp.save();
     }
